78b7b136afc7109210909cab95a6d53bba5d0326
George Buahin <139564185+VACOBUAHIG@users.noreply.github.com>
2024-05-08 19:27:41 -0400

    Update insert.py
    Added brackets to temp table name

--- mssql_dataframe/core/write/insert.py
+++ mssql_dataframe/core/write/insert.py
@@ -243,7 +243,7 @@ class insert:
 
         # insert data into source temporary table
         uid = "".join(random.choices(string.ascii_lowercase, k=4))  # nosec B311
-        temp_name = f"##__source_{table_name}_{uid}"
+        temp_name = f"[##__source_{table_name}_{uid}]"
         columns = list(dataframe.columns)
         if any(dataframe.index.names):
             columns = list(dataframe.index.names) + columns
